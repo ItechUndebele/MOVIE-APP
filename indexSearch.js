cd2ef3834ee438c09766c31e3c89093e8ab45a77
ItechUndebele <techperiodd@gmail.com>
2023-08-30 13:37:07 +0200

    movie app

--- indexSearch.js
+++ indexSearch.js
@@ -3,6 +3,7 @@ const searchInputValue = document.querySelector('#searchInputValue');
 const searchApiSpace = document.querySelector('#search--api--space');
 const nextButtonFor = document.querySelector('#next--button--for');
 const prevButtonFor = document.querySelector('#prev--button--for');
+const axception = document.querySelector('#axception');
 
 let search;
 let search_page = 1;
@@ -10,7 +11,7 @@ const key = '02e814d88a3e5259d80058c8c083f939';
 
 //SEARCH
 async function searchFunction(){
-
+  try{
     const url =`https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${key}&page=${search_page}`;
     const api = await fetch(url);
     const jsonData = await api.json();
@@ -24,11 +25,16 @@ async function searchFunction(){
         <img src = ${`https://image.tmdb.org/t/p/w500/${index.poster_path}`} id="search--api--img"/>
         </div>
         `  
-    ).join(''); 
+    ).join('');
+  } catch(TypeError){
+           axception.innerHTML = `0 match found`;
+  } 
   
   }
   
   
+
+
   //SEARCH--INPUT
   searchInputValue.addEventListener('keyup', (e)=>{
       search = e.target.value.toLowerCase();
